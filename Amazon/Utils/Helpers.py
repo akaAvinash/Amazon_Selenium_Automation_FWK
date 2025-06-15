efe2ec06f0b0b0a123e069660725663c84bd2792
Avinash <avinashkumar0101@gmail.com>
2025-06-16 00:00:11 +0530

    Fix: Added Chrome options to support headless CI in GitHub Actions

--- Amazon/Utils/Helpers.py
+++ Amazon/Utils/Helpers.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
-from Amazon.Utils.Constants import Amazon
+from selenium.webdriver.chrome.options import Options
 
 class Helper:
-
     def __init__(self):
-        self.target_url = Amazon.TARGET_URL
         self.driver = None
+        self.target_url = "https://www.amazon.in"  # Or whatever your default URL is
 
     def get_driver(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless=new")  # New headless mode for Chrome >=109
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--disable-dev-tools")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get(self.target_url)
         self.driver.maximize_window()
         return self.driver
